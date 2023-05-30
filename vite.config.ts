1e0948eab0c9a9971d25223c64e64a08ac61750e
CheIby <87913133+CheIby@users.noreply.github.com>
2023-05-30 14:28:31 +0700

    try enable hot reload but not work

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 3000 },
-  plugins: [react()],
+  server: { 
+    port: 3000,
+    watch :{
+      usePolling: true
+    }
+  },
+  plugins: [react({
+    include: "**/*.tsx",
+  })],
 });
